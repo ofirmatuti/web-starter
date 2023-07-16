3031d980aeadaf89dc2fbe50a1825c0ae4781779
Ofir Matuti <ofir.matuti@doubleverify.com>
2023-07-16 20:29:43 +0300

    initial commit

--- modules/client/components/Home.js
+++ modules/client/components/Home.js
@@ -4,7 +4,15 @@ import './Home.css'
 class Home extends React.Component {
   render() {
     return (
-      <div>go!</div>
+        <h1>
+
+          <span class="heart">&hearts;</span>
+          I Love You, Nuni!
+          <span class="heart">&hearts;</span>
+        <a>
+      <img src="https://m.media-amazon.com/images/I/41sWkH9sFEL._AC_UL400_.jpg" alt="Image of Love" />
+        </a>
+        </h1>
     )
   }
 }
